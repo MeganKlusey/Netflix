c2a73522132ddbf42f0f02a5a3b832aae378a71f
MeganKlusey <megan.klusey@hotmail.co.uk>
2020-08-25 19:07:37 +0100

    Add files via upload

---
+++ Netflix/script.js
@@ -0,0 +1,141 @@
+// drop down buttons
+
+function dropDown1() {
+	var x1 = document.getElementById('dd1');
+	var y1 = document.getElementById('plus1');
+	var x2 = document.getElementById('dd2');
+	var x3 = document.getElementById('dd3');
+	var x4 = document.getElementById('dd4');
+	var x5 = document.getElementById('dd5');
+	var y2 = document.getElementById('plus2');
+	var y3 = document.getElementById('plus3');
+	var y4 = document.getElementById('plus4');
+	var y5 = document.getElementById('plus5');
+	if (x1.style.display === 'block') {
+		x1.style.display = 'none';
+		y1.innerHTML = '＋';
+	} else {
+		x1.style.display = 'block';
+		y1.innerHTML = '✕';
+		x2.style.display = 'none';
+		x3.style.display = 'none';
+		x4.style.display = 'none';
+		x5.style.display = 'none';
+		y2.innerHTML = '＋';
+		y3.innerHTML = '＋';
+		y4.innerHTML = '＋';
+		y5.innerHTML = '＋';
+	}
+}
+
+function dropDown2() {
+	var x2 = document.getElementById('dd2');
+	var y2 = document.getElementById('plus2');
+	var x1 = document.getElementById('dd1');
+	var x3 = document.getElementById('dd3');
+	var x4 = document.getElementById('dd4');
+	var x5 = document.getElementById('dd5');
+	var y1 = document.getElementById('plus1');
+	var y3 = document.getElementById('plus3');
+	var y4 = document.getElementById('plus4');
+	var y5 = document.getElementById('plus5');
+	if (x2.style.display === 'block') {
+		x2.style.display = 'none';
+		y2.innerHTML = '＋';
+	} else {
+		x2.style.display = 'block';
+		y2.innerHTML = '✕';
+		x1.style.display = 'none';
+		x3.style.display = 'none';
+		x4.style.display = 'none';
+		x5.style.display = 'none';
+		y1.innerHTML = '＋';
+		y3.innerHTML = '＋';
+		y4.innerHTML = '＋';
+		y5.innerHTML = '＋';
+	}
+}
+
+function dropDown3() {
+	var x3 = document.getElementById('dd3');
+	var y3 = document.getElementById('plus3');
+	var x1 = document.getElementById('dd1');
+	var x2 = document.getElementById('dd2');
+	var x4 = document.getElementById('dd4');
+	var x5 = document.getElementById('dd5');
+	var y1 = document.getElementById('plus1');
+	var y2 = document.getElementById('plus2');
+	var y4 = document.getElementById('plus4');
+	var y5 = document.getElementById('plus5');
+	if (x3.style.display === 'block') {
+		x3.style.display = 'none';
+		y3.innerHTML = '＋';
+	} else {
+		x3.style.display = 'block';
+		y3.innerHTML = '✕';
+		x1.style.display = 'none';
+		x2.style.display = 'none';
+		x4.style.display = 'none';
+		x5.style.display = 'none';
+		y1.innerHTML = '＋';
+		y2.innerHTML = '＋';
+		y4.innerHTML = '＋';
+		y5.innerHTML = '＋';
+	}
+}
+
+function dropDown4() {
+	var x4 = document.getElementById('dd4');
+	var y4 = document.getElementById('plus4');
+	var x1 = document.getElementById('dd1');
+	var x2 = document.getElementById('dd2');
+	var x3 = document.getElementById('dd3');
+	var x5 = document.getElementById('dd5');
+	var y1 = document.getElementById('plus1');
+	var y2 = document.getElementById('plus2');
+	var y3 = document.getElementById('plus3');
+	var y5 = document.getElementById('plus5');
+	if (x4.style.display === 'block') {
+		x4.style.display = 'none';
+		y4.innerHTML = '＋';
+	} else {
+		x4.style.display = 'block';
+		y4.innerHTML = '✕';
+		x1.style.display = 'none';
+		x2.style.display = 'none';
+		x3.style.display = 'none';
+		x5.style.display = 'none';
+		y1.innerHTML = '＋';
+		y2.innerHTML = '＋';
+		y3.innerHTML = '＋';
+		y5.innerHTML = '＋';
+	}
+}
+
+function dropDown5() {
+	var x5 = document.getElementById('dd5');
+	var y5 = document.getElementById('plus5');
+	var x1 = document.getElementById('dd1');
+	var x2 = document.getElementById('dd2');
+	var x3 = document.getElementById('dd3');
+	var x4 = document.getElementById('dd4');
+	var y1 = document.getElementById('plus1');
+	var y2 = document.getElementById('plus2');
+	var y3 = document.getElementById('plus3');
+	var y4 = document.getElementById('plus4');
+	if (x5.style.display === 'block') {
+		x5.style.display = 'none';
+		y5.innerHTML = '＋';
+	} else {
+		x5.style.display = 'block';
+		y5.innerHTML = '✕';
+		x1.style.display = 'none';
+		x2.style.display = 'none';
+		x3.style.display = 'none';
+		x4.style.display = 'none';
+		y1.innerHTML = '＋';
+		y2.innerHTML = '＋';
+		y3.innerHTML = '＋';
+		y4.innerHTML = '＋';
+	}
+}
